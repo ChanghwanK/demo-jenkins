99e2c114308c8ed087e0135c04778e6e88b01de9
DevBloo <devchanghwan@gmail.com>
2021-06-05 12:12:27 +0900

    add: Jenkins CI Build 확인을 위한 Test 코드 작성

---
+++ src/test/java/com/bloo/jenkins/demojenkinsapp/controller/HomeControllerTest.java
@@ -0,0 +1,37 @@
+package com.bloo.jenkins.demojenkinsapp.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * @Created by Bloo
+ * @Date: 2021/06/05
+ */
+
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName ("")
+    @Test
+    void hello_test () throws Exception {
+        // given
+        // when
+        mockMvc.perform(get("/"))
+            .andExpect(content().string("Hello Bloo"))
+            .andExpect(status().isOk());
+        // then
+
+    }
+}
